dfd4674a1d890b0a70297f9661ae9a6679b441af
anathayna <anathaynafranca@gmail.com>
2020-05-10 02:08:35 -0300

    start project

---
+++ alumia.playground/Sources/Model/Elements.swift
@@ -0,0 +1,89 @@
+import Foundation
+
+public class Word {
+    public let body: [String: String] = [
+        "heart": "❤️",
+        "eyes": "👀",
+        "tongue": "👅",
+        "gloves": "🧤",
+        "hand": "✋🏽",
+        "nose": "👃🏽",
+        "ear": "👂🏽",
+        "mouth": "👄",
+        "tooth": "🦷",
+        "foot": "🦶🏽",
+        "leg": "🦵🏼",
+        "arm": "💪🏼"
+    ]
+    public let clothing: [String: String] = [
+        "shoe": "👞",
+        "pants": "👖",
+        "shirt": "👕",
+        "hat": "🧢",
+        "kimono": "👘",
+        "crown": "👑",
+        "ring": "💍",
+        "scarf": "🧣",
+        "dress": "👗",
+        "bag": "🎒",
+        "socks": "🧦",
+        "bikini": "👙"
+    ]
+    public let food: [String: String] = [
+        "banana": "🍌",
+        "apple": "🍎",
+        "pear": "🍐",
+        "peach": "🍑",
+        "lemon": "🍋",
+        "carrot": "🥕",
+        "pizza": "🍕",
+        "bread": "🍞",
+        "taco": "🌮",
+        "sushi": "🍣",
+        "egg": "🥚",
+        "coffee": "☕️"
+    ]
+    public let nature: [String: String] = [
+        "fish": "🐟",
+        "bear": "🐻",
+        "dog": "🐶",
+        "cat": "🐱",
+        "monkey": "🐵",
+        "tiger": "🐯",
+        "cow": "🐮",
+        "horse": "🐴",
+        "pig": "🐷",
+        "whale": "🐳",
+        "ant": "🐜",
+        "sun": "☀️"
+    ]
+    public let object: [String: String] = [
+        "bus": "🚎",
+        "car": "🚗",
+        "ball": "⚽️",
+        "clock": "⏰",
+        "balloon": "🎈",
+        "book": "📕",
+        "key": "🔑",
+        "hammer": "🔨",
+        "money": "💵",
+        "letter": "✉️",
+        "pencil": "✏️",
+        "bed": "🛏"
+    ]
+    public let mixed: [String: String] = [
+        "happy": "😄",
+        "smile": "😁",
+        "cry": "😢",
+        "love": "😍",
+        "sleep": "😴",
+        "kiss": "😚",
+        "sick": "🤒",
+        "sad": "☹️",
+        "angry": "😡",
+        "baby": "👶🏼",
+        "man": "👨🏻",
+        "woman": "👩🏽‍🦱"
+    ]
+    public init() {}
+}
